6364cf104e84684cd8ac7252c356ccdfb4a5ff0b
Rajeev Jha <jha.rajeev@gmail.com>
2026-07-31 23:17:52 +0530

    add postgres tables for hindi words
    - add postgres schema for hindi words
    - add option to store words in database during
     file processing
    - make changes to match nomenclature changes

--- hindi/words.py
+++ hindi/words.py
@@ -1,12 +1,41 @@
+import logging
+import os 
 import json
+import psycopg
+from config import AppConfig, get_logger_config
+from config import DatabaseType, get_database_config
 
 
-def get_capitalized_synonyms(english_raw_string):
-    if not english_raw_string:
+logger = logging.getLogger("main." + __name__)
+
+
+
+def _get_database_conn_string():
+    logger = logging.getLogger("main." + __name__)
+    # 4. Resolve the targeted active runtime environment config block
+    env_mode = os.environ.get("XAPI_ENV_MODE", "DEV").upper()
+    db_type = DatabaseType.PRODUCTION if env_mode == "PRODUCTION" else DatabaseType.DEV
+    db_config = get_database_config(db_type)
+
+    # 5. Build positional format connection credentials
+    DB_URI = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
+        db_config.db_user,
+        db_config.db_password,
+        db_config.db_host,
+        db_config.db_port,
+        db_config.db_name
+    )
+
+    logger.info("database URI -> " + DB_URI)
+    return DB_URI
+
+
+def _capitalize_english_word(raw_string):
+    if not raw_string:
         return []
     
     # 1. Split by commas to get each separate synonym
-    raw_list = english_raw_string.split(',')
+    raw_list = raw_string.split(',')
     
     processed_list = []
     for item in raw_list:
@@ -19,10 +48,92 @@ def get_capitalized_synonyms(english_raw_string):
     return processed_list
 
 
+def _store_root_word(conn: psycopg.Connection, token: str) ->str:
+    query = """
+            INSERT INTO hindi_master(token)
+            VALUES (%s)
+            ON CONFLICT(token) 
+            DO UPDATE SET token = EXCLUDED.token
+            RETURNING hindi_uuid;
+        """
+    with conn.cursor() as cur:
+        cur.execute(query, (token,))
+        row = cur.fetchone()
+        return row[0] if row else None
 
-def parse_file(file_name):   
-    game_data = []
-    seen_hindi_words = set()  # Set to track and prevent duplicate entries
+
+def _store_english_word(conn: psycopg.Connection, token: str) ->str:
+    query = """
+            INSERT INTO english_master(token)
+            VALUES (%s)
+            ON CONFLICT(token) 
+            DO UPDATE SET token = EXCLUDED.token
+            RETURNING english_uuid;
+        """
+    with conn.cursor() as cur:
+        cur.execute(query, (token,))
+        row = cur.fetchone()
+        return row[0] if row else None
+
+
+def _store_hindi_english_map(conn: psycopg.Connection, hindi_uuid: str, english_uuid):
+    query = """
+            INSERT INTO hindi_english_map(hindi_uuid, english_uuid)
+            VALUES (%s, %s)
+            ON CONFLICT(hindi_uuid, english_uuid) 
+            DO NOTHING ;
+        """
+    with conn.cursor() as cur:
+        cur.execute(query, (hindi_uuid, english_uuid))
+
+
+def _store_hindi_hindi_map(conn: psycopg.Connection, source_uuid: str, target_uuid):
+    query = """
+            INSERT INTO hindi_hindi_map(source_uuid, target_uuid)
+            VALUES (%s, %s)
+            ON CONFLICT(source_uuid, target_uuid) 
+            DO NOTHING ;
+        """
+    with conn.cursor() as cur:
+        cur.execute(query, (source_uuid, target_uuid))
+
+
+def store_word_list(word_list):
+    db_conn_string = _get_database_conn_string()
+    with psycopg.connect(db_conn_string) as conn:
+        for item in word_list:
+            root_word = item["root"]
+            english_words = item["root_english"]
+            root_synonyms = item["root_synonyms"]
+            try:
+                root_uuid = _store_root_word(conn, root_word)
+                for english_word in english_words:
+                    english_uuid = _store_english_word(conn, english_word)
+                    _store_hindi_english_map(conn, root_uuid, english_uuid)
+
+                synonym_set = set()
+                synonym_set.add(root_uuid)
+
+                for synonym in root_synonyms:
+                    synonym_uuid = _store_root_word(conn, synonym)
+                    synonym_set.add(synonym_uuid)
+
+                for item_uuid in synonym_set:
+                    map_set = set(synonym_set)
+                    map_set.discard(item_uuid)
+                    for pair_uuid in map_set:
+                        _store_hindi_hindi_map(conn, item_uuid, pair_uuid)
+
+                conn.commit()
+            except Exception as e:
+                conn.rollback()
+                logger.exception(f"error happened. database changes are rolled back.")
+                raise e 
+
+
+def process_file(file_name: str, dump_json=False, store_in_db=False):
+    word_list = []
+    seen_root_words = set()
 
     with open(file_name, 'r', encoding='utf-8') as file:
         for line in file:
@@ -30,44 +141,64 @@ def parse_file(file_name):
                 continue
 
             parts = line.split(';')
+            # Hindi source Word (Single string)
+            root_word = parts[0].strip()
             
-            # Group 1: Hindi Word (Single string)
-            hindi_word = parts[0].strip()
+            # English equivalent words (Converted to Array)
+            raw_english = parts[1].strip() if len(parts) > 1 else ""
+            root_english = _capitalize_english_word(raw_english)
             
-            # Group 2: English Synonyms (Converted to Array)
-            english_raw = parts[1].strip() if len(parts) > 1 else ""
-            english_synonyms = get_capitalized_synonyms(english_raw)
+            # Hindi Synonyms (Converted to Array)
+            raw_synonyms = parts[2].strip() if len(parts) > 2 else ""
+            root_synonyms = [item.strip() for item in raw_synonyms.split(',') if item.strip()] if raw_synonyms else []
             
-            
-            # Group 3: Hindi Synonyms (Converted to Array)
-            hindi_raw = parts[2].strip() if len(parts) > 2 else ""
-            hindi_synonyms = [item.strip() for item in hindi_raw.split(',') if item.strip()] if hindi_raw else []
-            
-            # 1. Add the primary main Hindi word if it hasn't been processed yet
-            if hindi_word and hindi_word not in seen_hindi_words:
-                seen_hindi_words.add(hindi_word)
-                game_data.append({
-                    "hindi": hindi_word,
-                    "english_synonyms": english_synonyms,
-                    "hindi_synonyms": hindi_synonyms
+            # Add the source Hindi word if it hasn't been processed yet
+            if root_word and root_word not in seen_root_words:
+                seen_root_words.add(root_word)
+                word_list.append({
+                    "root": root_word,
+                    "root_english": root_english,
+                    "root_synonyms": root_synonyms
                 })
             
             # 2. Loop through each Hindi synonym and create a standalone object entry
-            for synonym in hindi_synonyms:
-                if synonym and synonym not in seen_hindi_words:
-                    seen_hindi_words.add(synonym)
+            for synonym in root_synonyms:
+                if synonym and synonym not in seen_root_words:
+                    seen_root_words.add(synonym)
                     # Convert to a set to easily add/remove items
-                    synonym_set = set(hindi_synonyms)
+                    synonym_set = set(root_synonyms)
                     synonym_set.discard(synonym)  # Safely remove the word itself if present
-                    synonym_set.add(hindi_word)   # Add the original primary word as a synonym
+                    synonym_set.add(root_word)   # Add the original primary word as a synonym
                     
-                    game_data.append({
-                        "hindi": synonym,
-                        "english_synonyms": english_synonyms,  # Inherits same English meanings
-                        "hindi_synonyms": list(synonym_set)     
+                    word_list.append({
+                        "root": synonym,
+                        "root_english": root_english,  # Inherits same English meanings
+                        "root_synonyms": list(synonym_set)     
                     })
-    
-    with open("words.json", 'w', encoding='utf-8') as json_file:
-        json.dump(game_data, json_file, ensure_ascii=False, indent=4)
 
-parse_file("words.txt")
+    print(f"processed file {file_name}")
+    if dump_json:
+        with open("words.json", 'w', encoding='utf-8') as json_file:
+            json.dump(word_list, json_file, ensure_ascii=False, indent=4)
+        print(f"wrote to file words.json")
+
+    if store_in_db:
+        store_word_list(word_list)
+
+
+
+def do_main():
+
+    AppConfig.load()
+    log_config = get_logger_config("global")
+    AppConfig.init_logging(log_file=log_config.log_file, log_level=log_config.log_level)
+    logger.info(f"Hindi words program loaded...")
+    process_file("words.txt", store_in_db=True, dump_json=True)
+
+if __name__ == "__main__":
+    do_main()
+
+
+
+
+    
